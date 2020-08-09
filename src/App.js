833c4dbc5f0871fab11e7a5d815d21630a1a2d73
Ilyas Kussainov <ilyask@Ilyass-MacBook-Pro.local>
2020-08-09 17:08:36 -0400

    Displaying raw route data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data'
 
 class App extends Component {
   render() {
+    const routeTableData = data.routes.map((route) => (
+      <tr>
+        <td>{route.airline}</td>
+        <td>{route.src}</td>
+        <td>{route.dest}</td>
+      </tr>
+    ))
+
     return (
       <div className="app">
         <header className="header">
@@ -12,6 +21,19 @@ class App extends Component {
           <p>
             Welcome to the app!
           </p>
+
+          <table class="routes-table">
+            <thead>
+              <tr>
+                <th>Airline</th>
+                <th>Source</th>
+                <th>Destination</th>
+              </tr>
+            </thead>
+            <tbody>
+              {routeTableData}
+            </tbody>
+          </table>
         </section>
       </div>
     );
